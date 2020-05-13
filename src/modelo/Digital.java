022ba8fd4e2d1f0b0fdd1ae9742ff5fefe28a782
leoAlvTor <calvaradot1@est.ups.edu.ec>
2020-05-13 16:27:28 -0500

    Commit

--- src/modelo/Digital.java
+++ src/modelo/Digital.java
@@ -7,6 +7,16 @@ public class Digital extends Libro{
     }
 
     @Override
+    public double calcularPrecioFijo() {
+        return 0;
+    }
+
+    @Override
+    public double calcularPrecioEnvio() {
+        return 0;
+    }
+
+    @Override
     public double calcularPrecioFinal() {
         return 2;
     }
--- src/modelo/Impreso.java
+++ src/modelo/Impreso.java
@@ -3,6 +3,16 @@ package modelo;
 public class Impreso extends Libro{
 
     @Override
+    public double calcularPrecioFijo() {
+        return 0;
+    }
+
+    @Override
+    public double calcularPrecioEnvio() {
+        return 0;
+    }
+
+    @Override
     public double calcularPrecioFinal() {
         return 0;
     }
